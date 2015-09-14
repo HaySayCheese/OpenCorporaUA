6e0806c70639d573b0363aecd2f01c4c644b7c2b
hsc <dima.chizhevsky@gmail.com>
2015-09-14 11:38:18 +0300

    static settings

--- OpenCorporaUA/settings.py
+++ OpenCorporaUA/settings.py
@@ -97,7 +97,4 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_URL = '127.0.0.1/open_corpora_ua_static/'
